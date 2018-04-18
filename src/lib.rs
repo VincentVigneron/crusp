e83b86afb47f9c8a4f5c995e56986f4be9492fa7
Vigneron <vigneron.vincent.49@gmail.com>
2018-04-18 11:45:48 +0200

    Remove boxed_clone

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,6 @@ extern crate snowflake;
 // The order of modules matter !!!! (macros)
 
 #[macro_use]
-pub mod boxed_clone;
-#[macro_use]
 pub mod variables;
 pub mod constraints;
 pub mod branchers;
