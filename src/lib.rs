f993b71affdd677c79cb84a102962039b80698f0
Vigneron <vigneron.vincent.49@gmail.com>
2018-04-25 19:21:42 +0200

    Remove trace macros.

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,10 @@ extern crate snowflake;
 
 // The order of modules matter !!!! (macros)
 
-trace_macros!(false);
 #[macro_use]
 mod macros;
 #[macro_use]
 pub mod variables;
-trace_macros!(false);
 pub mod constraints;
-trace_macros!(false);
 pub mod branchers;
 pub mod spaces;
